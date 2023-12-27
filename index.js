dd12f936eeeefbca122871763612a64bf1d6a5ca
Nikita Van <nikitaszvan@gmail.com>
2023-12-27 14:01:36 -0500

    add styling

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ if (cluster.isPrimary) {
     const numCPUs = availableParallelism();
     for (let i = 0; i < numCPUs; i++) {
       cluster.fork({
-        PORT: 3000 + i
+        PORT: 3000 + i,
+        WORKER_ID: i + 1
       });
     }
   
@@ -27,39 +28,44 @@ const io = new Server(server,  {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-let number_user = 1;
+const workerId = process.env.WORKER_ID;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    socket.username = `User${number_user}`;
-    socket.broadcast.emit('chat message', { user: '', message: `(${socket.id}) ${socket.username} has joined the chat`});
-    number_user += 1;
+  try {
+  socket.username = `User${(workerId - 1)}`;
+  socket.broadcast.emit('chat message', { user: '', message: `(${socket.id.slice(-5)}) ${socket.username} has joined the chat` });
 
-    socket.on('change username', (newUsername) => {
-        const oldUsername = socket.username || 'User';
-        socket.username = newUsername;
-        socket.broadcast.emit('chat message', { user: '', message: `(${socket.id}) ${oldUsername} changed their username to ${socket.username}`});
-    });
 
-    socket.on('chat message', (data) => {
-        socket.message = data.message;
-        io.emit('chat message', { user: socket.username, message: data.message, uniqueID: socket.id, submit: true });
-    });
+  socket.on('change username', (newUsername) => {
+    const oldUsername = socket.username || `User${(workerId - 1)}`;
+    socket.username = newUsername;
+    socket.broadcast.emit('chat message', { user: '', message: `(${socket.id.slice(-5)}) ${oldUsername} changed their username to ${socket.username}`});
+  });
 
-    socket.on('typing notification', (data) => {
-            socket.broadcast.emit('chat message', { user: '', message: `(${socket.id}) ${socket.username} is typing ...`, inputContent: data.message, submit: false});
-        }
-    );
+  socket.on('chat message', (data) => {
+    const isLocalUser = data.workerId ==- workerId;
+      socket.message = data.message;
+      io.emit('chat message', { user: socket.username, message: data.message, uniqueID: `(${socket.id.slice(-5)})`, submit: true, checkLocalUser: isLocalUser });
+  });
 
-    socket.on('disconnect', () => {
-        io.emit('chat message', { user: '', message: ` ${socket.username} has disconnected` });
-      });
+  socket.on('typing notification', (data) => {
+      socket.broadcast.emit('chat message', { user: '', message: `(${socket.id.slice(-5)}) ${socket.username} is typing ...`, inputContent: data.message, submit: false});
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('chat message', { user: '', message: ` (${socket.id.slice(-5)}) ${socket.username} has disconnected` });
+  });
+}
+catch (e) {
+  console.log(e);
+}
+
 });
-
-const port = process.env.PORT;
 
   server.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
