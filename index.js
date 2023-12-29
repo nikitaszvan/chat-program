3861ba4d99b2a61e001bdd95a683ce22329513b0
Nikita Van <nikitaszvan@gmail.com>
2023-12-28 22:26:35 -0500

    fix errors

--- index.js
+++ index.js
@@ -38,30 +38,32 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   try {
-  socket.username = `User${(workerId - 1)}`;
+  socket.username = `User${(workerId)}`;
   const d = new Date(Date.now());
   socket.emit('chat message', {message: `${daysOfWeek[d.getDay()]} - ${d.getHours()}:${d.getMinutes()}`, class: 'general'});
-  socket.broadcast.emit('chat message', { user: '', message: `${socket.username} has joined the chat`, class: 'general'});
+  socket.broadcast.emit('chat message', { user: '', message: `${socket.username} has joined the chat`, dataInput: false, class: 'general'});
 
   socket.on('change username', (newUsername) => {
-    const oldUsername = socket.username || `User${(workerId - 1)}`;
+    const oldUsername = socket.username;
     socket.username = newUsername;
     io.emit('worker_id', { workerId: workerId, newUsername: socket.username});
     socket.broadcast.emit('chat message', { user: '', message: `${oldUsername} changed their username to ${socket.username}`, class: 'general'});
   });
 
   socket.on('chat message', (data) => {
-      socket.emit('worker_id', { workerId: workerId });
-      io.emit('chat message', { user: socket.username, message: data.message, uniqueID: `${socket.id.slice(-5)}`, submit: true, workerId: workerId});
+      io.emit('worker_id', { workerId: workerId, typingArray: data.typingArray, inputContent: false});
+      socket.emit('chat message', { user: socket.username, message: data.message, uniqueID: `${socket.id.slice(-5)}`, submit: true, class: 'local', workerId: workerId});
+      socket.broadcast.emit('chat message', { user: socket.username, message: data.message, uniqueID: `${socket.id.slice(-5)}`, class: 'non-local', workerId: workerId});
   });
 
   socket.on('typing notification', (data) => {
       io.emit('worker_id', { workerId: workerId, inputContent: data.message, typingArray: data.typingArray, submit: false, class: 'general'});
-      socket.broadcast.emit('chat message', { user: '', message: `${socket.username} is typing ...` });
+      io.emit('chat message', { typingArray: data.typingArray, type: 'typing notification', class: 'general'});
   });
 
   socket.on('disconnect', () => {
-    socket.emit('chat message', { user: '', message: ` ${socket.username} has disconnected`, class: 'general'});
+    io.emit('worker_id', { workerId: workerId, disconnect: true});
+    socket.broadcast.emit('chat message', { user: '', message: ` ${socket.username} has disconnected`, class: 'general', type: 'typing notification'});
   });
 }
 catch (e) {
