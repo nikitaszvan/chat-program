1c85f50457a116fd7c962aa6b47a76b1c41415e4
Nikita Van <nikitaszvan@gmail.com>
2024-01-06 22:34:59 -0500

    Update MyForm.jsx

--- client/src/components/MyForm.jsx
+++ client/src/components/MyForm.jsx
@@ -4,17 +4,16 @@ import { socket } from '../socket.js';
 export function MyForm() {
   let [messages, setMessages] = useState([]);
   const [value, setValue] = useState('');
+  const [lastSenderId, setLastSenderId] = useState(null);
 
   useEffect(() => {
     if (socket) {
       socket.on("message", (value) => {
         setMessages([...messages, value]);
         let element = document.getElementById("messages");
-        console.log(socket.id);
         element.scrollTop = element.scrollHeight;
       });
     }
-
     return () => {
       socket?.off("message");
     };
@@ -24,18 +23,19 @@ export function MyForm() {
     e.preventDefault();
     if (value && socket) {
       socket.emit("sendMessage", { user: socket.id, msg: value });
+      setLastSenderId(socket.id);
       setValue("");
     }
   }
 
   function renderMessage(item, index) {
     return (
-      <li key={index} className={item.user === socket.id? 'local' : 'non-local'}>
-        {item.msg}
+      <li key={index} className={`${item.user === socket.id? 'local' : 'non-local'} ${item.user !== lastSenderId ? 'first-message' : ''}`} >
+        {item.msg} : {item.user} : {lastSenderId}
       </li>
     );
   }
-
+  
   return (
     <>
       <ul id="messages">
